715fa77f2e6f48384563e985a2622e1daaddd7c6
merwin mathew <merwinmathew3@gmail.com>
2022-12-25 23:00:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+
+
+const app = express()
+const mongoose =require('mongoose')
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error',(error)=>console.error(error))
+db.once('open',()=>console.log('connected to database'))
+
+app.listen(3000, ()=> console.log('server started '))
